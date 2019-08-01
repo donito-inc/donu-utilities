e3439430d23f87c3ea67b4fb9a662601f5d51b2e
ahmedbodi <ahmed_bodi@msn.com>
2019-08-02 00:18:30 +0100

    Switch to QT5

--- donu-builder.sh
+++ donu-builder.sh
@@ -6,7 +6,8 @@
 
 echo "Installing Coin Dependencies"
 sudo apt-get update
-sudo apt-get install -y build-essential libcurl4-openssl-dev libdb5.3-dev libdb5.3++-dev libtool autoconf autotools-dev automake pkg-config libssl-dev libevent-dev bsdmainutils python3 libboost-all-dev libprotobuf-dev protobuf-compiler libqt4-dev libcanberra-gtk-module
+#sudo apt-get install -y build-essential libcurl4-openssl-dev libdb5.3-dev libdb5.3++-dev libtool autoconf autotools-dev automake pkg-config libssl-dev libevent-dev bsdmainutils python3 libboost-all-dev libprotobuf-dev protobuf-compiler libqt4-dev libcanberra-gtk-module
+sudo apt-get install build-essential libtool autotools-dev automake pkg-config bsdmainutils python3 libssl-dev libevent-dev libboost-system-dev libboost-filesystem-dev libboost-chrono-dev libboost-test-dev libboost-thread-dev  libminiupnpc-dev libzmq3-dev libqt5gui5 libqt5core5a libqt5dbus5 qttools5-dev qttools5-dev-tools libprotobuf-dev protobuf-compiler libqrencode-dev
 
 echo "Installing Git"
 sudo apt-get install -y git
